c49f7769eef169ddbc4cbf909487936d78910378
Abu Hasnat <hasnat.tushar@gmail.com>
2023-05-18 01:42:37 -0500

    Project VM

---
+++ Models/ViewModel/ProjectVM.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace SD_340_W22SD_Final_Project_Group6.Models.ViewModel
+{
+    public class ProjectVM
+    {
+        public int Id { get; set; }
+        public string ProjectName { get; set; }
+        public List<SelectListItem> Users { get; set; }
+
+        public ProjectVM(List<ApplicationUser> users)
+        {
+            Users = new List<SelectListItem>();
+            foreach (var user in users)
+            {
+                Users.Add(new SelectListItem(user.UserName, user.Id));
+            }
+        }
+    }
+}
